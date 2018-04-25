62f4644a95f967f2abc4aee5114e20b43168c45c
Lee Henson <lee.m.henson@gmail.com>
2018-04-25 21:20:25 +0100

    simplerng tests

--- chapter6.ts
+++ chapter6.ts
@@ -1,7 +1,17 @@
+import * as prng from "seedrandom";
+
 export interface RNG {
   nextInt: () => [number, RNG];
 }
 
 export class SimpleRNG implements RNG {
-  nextInt: () => [number, RNG];
+  constructor(private readonly seed: number) {}
+
+  public nextInt(): [number, RNG] {
+    const newSeed = (this.seed * 0x5deece66d + 0xb) & 0xffffffffffff;
+    const nextRNG = new SimpleRNG(newSeed);
+    const n = Math.round(newSeed >>> 16);
+
+    return [n, nextRNG];
+  }
 }
--- tests/chapter6.test.ts
+++ tests/chapter6.test.ts
@@ -1,3 +1,14 @@
-describe("x", () => {
-  it("y", () => expect(true).toBeTruthy());
+import { SimpleRNG } from "../chapter6";
+
+describe("SimpleRNG", () => {
+  const rng42 = new SimpleRNG(42);
+
+  it("generates the same nextInt from the same seed", () => {
+    expect(rng42.nextInt()["0"]).toEqual(37597);
+    expect(rng42.nextInt()["0"]).toEqual(rng42.nextInt()["0"]);
+  });
+
+  it("generates a new nextInt from the returned RNG instance", () => {
+    expect(rng42.nextInt()["1"].nextInt()["0"]).not.toEqual(rng42.nextInt()["0"]);
+  });
 });
